3c18ac58da36c4a41fcaa30f6bee281990faadac
Simon Blanco <simon.blanco@mailbox.org>
2024-01-21 11:30:27 +0100

    ex2 testbench added (1proc timing maybe wrong)

---
+++ 2023-06-21/ex2_FSM_preambler/ex2_FSM_preambler.srcs/sim_1/new/FSM_preambler_tb.vhd
@@ -0,0 +1,83 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/21/2024 11:24:40 AM
+-- Design Name: 
+-- Module Name: testbench - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity testbench is
+--  Port ( );
+end testbench;
+
+architecture Behavioral of testbench is
+    
+    component FSM_preambler is
+        port(
+            start :     in  std_logic;
+            data_out :  out std_logic;
+            clk, rst :  in  std_logic
+         );
+    end component;
+    
+    signal start_s, data_out_s, clk_s, rst_s : std_logic;
+    constant clk_period : time := 20ns;
+    
+begin
+    uut0: FSM_preambler port map (start_s, data_out_s, clk_s, rst_s);
+    
+    process
+    begin
+        clk_s <= '0';
+        wait for clk_period/2;
+        clk_s <= '1';
+        wait for clk_period/2;
+    end process;
+    
+    
+    process
+    begin
+        -- Init
+        rst_s <= '1';
+        start_s <= '0';
+        wait for clk_period/4;
+        rst_s <= '0';
+        wait for clk_period;
+        
+        -- Start
+        start_s <= '1';
+        wait for clk_period;
+        start_s <= '0';
+        
+        wait for clk_period*10;
+        
+        wait;
+        
+    end process;
+
+end Behavioral;
